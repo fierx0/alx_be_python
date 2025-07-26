7e06e0133f65be89444e7e1302bbab7c14b3a4e9
abdellah idmhand <idmhand.abdellah97@gmail.com>
2025-07-26 11:04:04 +0000

    new

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,13 @@
 # arithmetic_operations.py
 
-def perform_operation (num1: float, num2: float, operation: str):
-    if perform_operation == 'add':
+def perform_operation(num1: float, num2: float, operation: str):
+    if operation == 'add':
         return num1 + num2
-    elif perform_operation == 'subtract':
+    elif operation == 'subtract':
         return num1 - num2
-    elif perform_operation == 'multiply':
+    elif operation == 'multiply':
         return num1 * num2
-    elif perform_operation == 'divide':
+    elif operation == 'divide':
         if num2 == 0:
             return "Error: Division by zero"
         return num1 / num2
